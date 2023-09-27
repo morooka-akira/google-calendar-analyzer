2a7beec2dab7cd7e0f5ceae5424b3eb7cf9c731e
Heiya <morooka.akira.inon29@gmail.com>
2023-09-27 22:04:20 +0900

    access_tokenをファイルに一時保存するようにした

--- src/oauth.rs
+++ src/oauth.rs
@@ -1,5 +1,6 @@
-use std::{collections::HashMap, fs};
+use std::{collections::HashMap, fs, io, path::Path, time::SystemTime};
 
+use chrono::{DateTime, Duration, FixedOffset, Utc};
 use oauth2::{
     basic::BasicClient, AuthUrl, ClientId, ClientSecret, CsrfToken, PkceCodeChallenge, RedirectUrl,
     Scope, TokenUrl,
@@ -7,7 +8,7 @@ use oauth2::{
 use reqwest::Client;
 use serde::{Deserialize, Serialize};
 
-#[derive(Debug, Deserialize)]
+#[derive(Debug, Deserialize, Serialize)]
 pub struct TokenResponse {
     pub access_token: String,
     pub token_type: String,
@@ -27,43 +28,97 @@ struct Installed {
     client_secret: String,
 }
 
-const TOKEN_ENDPOINT: &str = "https://oauth2.googleapis.com/token";
-
-async fn get_access_token_internal(
-    client_id: &str,
-    client_secret: &str,
-    auth_code: &str,
-    redirect_uri: &str,
-    code_verifier: &str,
-) -> Result<TokenResponse, reqwest::Error> {
-    let client = Client::new();
-
-    let mut params = HashMap::new();
-    params.insert("client_id", client_id);
-    params.insert("client_secret", client_secret);
-    params.insert("code", auth_code);
-    params.insert("redirect_uri", redirect_uri);
-    params.insert("grant_type", "authorization_code");
-    params.insert("code_verifier", code_verifier);
-
-    let response: TokenResponse = client
-        .post(TOKEN_ENDPOINT)
-        .form(&params)
-        .send()
-        .await?
-        .json()
-        .await?;
-
-    Ok(response)
+#[derive(Debug, Deserialize, Serialize)]
+pub struct TempToken {
+    pub access_token: String,
+    pub token_type: String,
+    pub expires_in: u64,
+    pub refresh_token: Option<String>,
+    pub scope: Option<String>,
+    pub created_at: String,
 }
 
-pub async fn get_access_token() -> Result<TokenResponse, reqwest::Error> {
-    let data = fs::read_to_string("credentials.json").unwrap();
-    let credentials: Credentials = serde_json::from_str(&data).unwrap();
+impl TempToken {
+    const TEMP_CREDENTIAL_PATH: &str = ".temp/token.json";
 
-    let client_id = &credentials.installed.client_id;
-    let client_secret = &credentials.installed.client_secret;
-    let redirect_uri = "urn:ietf:wg:oauth:2.0:oob";
+    fn load_to_file() -> Result<TempToken, io::Error> {
+        let token_data = fs::read_to_string(Self::TEMP_CREDENTIAL_PATH)?;
+        let token: TempToken = serde_json::from_str(&token_data)?;
+
+        Ok(token)
+    }
+
+    fn save_to_file(&self) -> Result<(), io::Error> {
+        let json = serde_json::to_string_pretty(self)?;
+
+        let path = Path::new(Self::TEMP_CREDENTIAL_PATH);
+        if let Some(dir) = path.parent() {
+            if !dir.exists() {
+                fs::create_dir_all(dir)?;
+            }
+        }
+
+        fs::write(path, json)?;
+        Ok(())
+    }
+
+    fn valid_token(&self) -> bool {
+        let datetime_with_offset =
+            DateTime::<FixedOffset>::parse_from_rfc3339(&self.created_at).unwrap();
+        let datetime_utc: DateTime<Utc> = datetime_with_offset.with_timezone(&Utc);
+        let now = Utc::now();
+
+        now < datetime_utc + Duration::seconds(self.expires_in as i64)
+    }
+}
+
+const TOKEN_ENDPOINT: &str = "https://oauth2.googleapis.com/token";
+const REDIRECT_URI: &str = "urn:ietf:wg:oauth:2.0:oob";
+
+pub async fn get_access_token() -> Result<TokenResponse, reqwest::Error> {
+    if let Ok(token) = TempToken::load_to_file() {
+        if token.valid_token() {
+            Ok(TokenResponse {
+                access_token: token.access_token,
+                token_type: token.token_type,
+                expires_in: token.expires_in,
+                refresh_token: token.refresh_token,
+                scope: token.scope,
+            })
+        } else {
+            todo!("refresh token");
+        }
+    } else {
+        let data = fs::read_to_string("credentials.json").unwrap();
+        let credentials: Credentials = serde_json::from_str(&data).unwrap();
+
+        let client_id = &credentials.installed.client_id;
+        let client_secret = &credentials.installed.client_secret;
+        let (pkce_verifier, auth_code) = get_auth_code(client_id, client_secret);
+
+        let token =
+            get_access_token_internal(client_id, client_secret, &auth_code, pkce_verifier.secret())
+                .await?;
+        save_to_tmpfile(&token);
+
+        Ok(token)
+    }
+}
+
+fn save_to_tmpfile(token: &TokenResponse) {
+    let date_time: DateTime<Utc> = SystemTime::now().into();
+    let credentials = TempToken {
+        access_token: token.access_token.clone(),
+        token_type: token.token_type.clone(),
+        expires_in: token.expires_in,
+        refresh_token: token.refresh_token.clone(),
+        scope: token.scope.clone(),
+        created_at: date_time.to_rfc3339(),
+    };
+    let _ = credentials.save_to_file();
+}
+
+fn get_auth_code(client_id: &str, client_secret: &str) -> (oauth2::PkceCodeVerifier, String) {
     let google_client_id = ClientId::new(client_id.to_string());
     let google_client_secret = ClientSecret::new(client_secret.to_string());
 
@@ -77,7 +132,7 @@ pub async fn get_access_token() -> Result<TokenResponse, reqwest::Error> {
         auth_url,
         Some(token_url),
     )
-    .set_redirect_uri(RedirectUrl::new(redirect_uri.to_string()).unwrap());
+    .set_redirect_uri(RedirectUrl::new(REDIRECT_URI.to_string()).unwrap());
 
     let (pkce_challenge, pkce_verifier) = PkceCodeChallenge::new_random_sha256();
 
@@ -89,25 +144,39 @@ pub async fn get_access_token() -> Result<TokenResponse, reqwest::Error> {
         .set_pkce_challenge(pkce_challenge)
         .url();
 
-    println!(
-        "Please open the following URL in your browser:\n{}",
-        auth_url
-    );
+    println!("次のURLをブラウザで開いて認証してください:\n{}", auth_url);
 
-    // コンソールからユーザーが入力した認証コードを取得
     let mut auth_code = String::new();
-    println!("Please enter the authorization code you received:");
+
+    println!("認証コードをペーストしてEnterを入力:");
     std::io::stdin().read_line(&mut auth_code).unwrap();
     let auth_code = auth_code.trim();
 
-    print!("auth_code: {}", auth_code);
+    (pkce_verifier, auth_code.to_string())
+}
 
-    return get_access_token_internal(
-        client_id,
-        client_secret,
-        auth_code,
-        redirect_uri,
-        pkce_verifier.secret(),
-    )
-    .await;
+async fn get_access_token_internal(
+    client_id: &str,
+    client_secret: &str,
+    auth_code: &str,
+    code_verifier: &str,
+) -> Result<TokenResponse, reqwest::Error> {
+    let client = Client::new();
+
+    let mut params = HashMap::new();
+    params.insert("client_id", client_id);
+    params.insert("client_secret", client_secret);
+    params.insert("code", auth_code);
+    params.insert("redirect_uri", REDIRECT_URI);
+    params.insert("grant_type", "authorization_code");
+    params.insert("code_verifier", code_verifier);
+
+    let response: TokenResponse = client
+        .post(TOKEN_ENDPOINT)
+        .form(&params)
+        .send()
+        .await?
+        .json()
+        .await?;
+    Ok(response)
 }
